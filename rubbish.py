a85275e66d407eff88ceaf332842b7ac076e1a0a
noelrodri <55175870+noelrodri@users.noreply.github.com>
2020-04-29 13:01:04 +0530

    Addicted7 download working
    Addicted7 subtitle working

--- rubbish.py
+++ rubbish.py
@@ -8,6 +8,20 @@ from xmlrpc.client import ServerProxy
 import base64
 import zlib
 import time
+from bs4 import BeautifulSoup
+import traceback
+import requests
+import socket
+from operator import itemgetter
+
+LANGUAGES = {
+    u'English': ('1', 'eng'),
+    u'French': ('8', 'fre'),
+    u'Greek': ('27', 'ell'),
+    u'Spanish': ('4', 'spa'),
+    u'Portuguese(Br)': ('10', 'pob'),
+    u'Italian': ('7', 'ita'),
+}
 
 
 def is_video_file(filename):
@@ -89,6 +103,51 @@ def clean_name(name):
     return name
 
 
+class NoInternetConnectionFound(Exception):
+    pass
+
+
+class IncorrectResponseRecieved(Exception):
+    pass
+
+
+class DailyDownloadLimitExceeded(Exception):
+    pass
+
+
+request_headers = {
+    'Host': 'www.addic7ed.com',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:14.0) Gecko/20100101 Firefox/14.0.1',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-us,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive',
+}
+
+
+def download_url_content(url, referer=None, timeout=10):
+    ''' Downloads and returns the contents of the given url.'''
+
+    if referer:
+        request_headers['Referer'] = referer
+    else:
+        request_headers['Referer'] = url
+
+    try:
+        x = requests.get(url, headers=request_headers, timeout=timeout)
+    except (requests.exceptions.Timeout, exceptions.ConnectionError, socket.timeout):
+        raise NoInternetConnectionFound
+
+    if x.status_code != 200:
+        raise IncorrectResponseRecieved
+
+    # print(x.content)
+    # if x.content.find('Daily Download count exceeded') != -1:
+    #     raise DailyDownloadLimitExceeded
+    # else:
+    return x.content
+
+
 def guess_file_data(filename):
     filename = clean_name(filename)
     matches_tvshow = tv_show_regex.match(filename)
@@ -142,171 +201,132 @@ def catch_all(site, files):
         print(e)
 
 
-LANGUAGES = {
-    u'English': ('1', 'eng'),
-    u'French': ('8', 'fre'),
-    u'Greek': ('27', 'ell'),
-    u'Spanish': ('4', 'spa'),
-    u'Portuguese(Br)': ('10', 'pob'),
-    u'Italian': ('7', 'ita'),
-}
-
-
 def save_subs(content, full_path, other_details=None):
     open(full_path, 'wb').write(content)
 
 
-class OpenSubtitles:
+class Addic7ed:
 
-    api_url = 'http://api.opensubtitles.org/xml-rpc'
-    login_token = None
-    server = None
+    host = 'http://www.addic7ed.com'
 
     def __init__(self, parent=None):
         self.stopping = False
 
+    def run(self):
+        print('Querying Addic7ed.com...', 'info')
+        for details_dict in self.files_list[:]:
+            if not self.stopping:
+                filename = details_dict['file_name']
+                save_to = details_dict['save_subs_to']
+
+                try:
+                    (searched_url, downloadlink) = self._query(filename)
+                    if downloadlink:
+                        subs = self.download_subtitles(searched_url, downloadlink, filename)
+                    else:
+                        print('Nothing found.', 'error')
+                        print(details_dict)
+                        continue
+
+                except NoInternetConnectionFound:
+                    print('No active Internet connection found. Kindly check and try again.', 'error')
+                except:  # utils.DailyDownloadLimitExceeded:
+                    for details_dict in self.files_list:
+                        (details_dict)
+                    raise
+                else:
+                    save_subs(subs, save_to)
+                    self.files_list.remove(details_dict)
+
     def process(self, files_list, lang='English'):
-        self.moviefiles = files_list
-        self.imdbid_to_hash = {}
-        self.lang = LANGUAGES[lang][1]
+        '''Given filename and the wished language, searches and downloads the best match found from Addic7ed.com'''
+        self.lang = lang
+        self.files_list = files_list
         if not self.stopping:
             self.run()
-
-    def __del__(self):
-        if self.login_token:
-            self.logout()
 
     def stopTask(self):
         self.stopping = True
 
-    def run(self):
+    def _listTeams(self, subteams, separators):
+        for sep in separators:
+            subteams = self._splitTeam(subteams, sep)
+        return set(subteams)
+
+    def _splitTeam(self, subteams, sep):
+        teams = []
+        for t in subteams:
+            teams += t.split(sep)
+        return teams
+
+    def _query(self, filename):
+        print('Searching Addic7ed.com for %s' % filename, 'info')
+        guessed_file_data = guess_file_data(filename)
+        name = guessed_file_data.get('name')
+        season = guessed_file_data.get('season')
+        episode = guessed_file_data.get('episode')
+        teams = guessed_file_data.get('teams')
+
+        lang_url = LANGUAGES[self.lang][0]
+        searchurl = '%s/serie/%s/%s/%s/%s' % (self.host, name, season, episode, lang_url)
+
+        name = name.lower().replace(' ', '_')
+        teams = set(teams)
+
+        best_match = None
+
+        page_html = download_url_content(searchurl)
+        if not page_html.strip():
+            return (searchurl, None)
+        soup = BeautifulSoup(page_html, features='lxml')
+        release_pattern = re.compile('Version (.+), ([0-9]+).([0-9])+ MBs')
+
         try:
-            if not self.login_token:
-                self.login()
-            self.search_subtitles()
-        except Exception as e:
-            print(e, "first exception")
+            sub_list = soup.findAll('td', {'class': 'NewsTitle', 'colspan': '3'})
+            result = []
 
-    def login(self):
-        '''Log in to OpenSubtitles.org'''
+            for subs in sub_list:
+                subteams = release_pattern.match(subs.contents[1].strip()).groups()[0].lower()
+                subteams = self._listTeams([subteams], ['.', '_', ' '])
+                langs_html = subs.find_next('td', {'class': 'language'})
+                statusTD = langs_html.find_next('td')
+                status = statusTD.text.strip()
+                links = statusTD.find_next('td').find_all('a')
+                link = '%s%s' % ('http://www.addic7ed.com', links[len(links) - 1]['href'])
 
-        self.server = ServerProxy(self.api_url, verbose=False)
+                b = {}
+                b['link'] = link
 
-        try:
-            resp = self.server.LogIn('', '', 'en', 'PySubD v2.0')
-            self.check_status(resp)
-            self.login_token = resp['token']
-        except Exception as e:
-            if e.args[0] == 11004:
-                print("No Internet Connection Found", 'error')
-            else:
-                print(str(e), 'error')
-
-    def logout(self):
-        '''Log out from OpenSubtitles'''
-
-        print('Logging out...', 'info')
-        resp = self.server.LogOut(self.login_token)
-        self.check_status(resp)
-
-    def check_status(self, resp):
-        '''Check the return status of the request.
-        Anything other than "200 OK" raises a UserWarning
-        '''
-
-        if resp['status'].upper() != '200 OK':
-            print('Response error')
-
-    def _query_opensubs(self, search):
-        results = []
-        while search[:500]:
-            if not self.stopping:
-                try:
-                    tempresp = self.server.SearchSubtitles(self.login_token, search[:500])
-                except Exception as e:
-                    if e.args[0] == 11004:
-                        print("No Internet Connection Found", 'error')
+                if status == 'Completed':
+                    b['completed'] = 1
+                    sub_quality = subs.parent.parent.find_all('a', {'class': 'buttonDownload'})[0].text
+                    if sub_quality == u'original' or sub_quality == u'most updated':
+                        b['best_match'] = 1
                     else:
-                        print(str(e), 'error')
-                    return results
-                if tempresp['data']:
-                    results.extend(tempresp['data'])
-                self.check_status(tempresp)
-                search = search[500:]
-            else:
-                return []
-
-        return results
-
-    def clean_results(self, results):
-        subtitles = {}
-        user_ranks = {'administrator': 1,
-                      'platinum member': 2,
-                      'vip member': 3,
-                      'gold member': 4,
-                      'trusted': 5,
-                      'silver member': 6,
-                      'bronze member': 7,
-                      'sub leecher': 8,
-                      '': 9, }
-
-        for result in results:
-            if result['SubBad'] != '1':
-                movie_hash = result.get('MovieHash')
-                if not movie_hash:
-                    movie_hash = self.imdbid_to_hash[int(result['IDMovieImdb'])]
-                subid = result['IDSubtitleFile']
-                downcount = int(result['SubDownloadsCnt'])
-                rating = float(result['SubRating'])
-                IDMovieImdb = int(result.get('IDMovieImdb', 0))
-
-                if rating and rating < 8:
-                    # Ignore poorly rated subtitles, while not
-                    # penalizing the ones that haven't yet been rated
-                    continue
-
-                user_rank = user_ranks[result['UserRank']]
-                subtitles.setdefault(movie_hash, []).append((subid, downcount, rating, user_rank, IDMovieImdb))
-
-        return subtitles
-
-    def search_subtitles(self):
-        search = []
-        cnt = Counter()
-        for video_file_details in self.moviefiles.values():
-            video_file_details['sublanguageid'] = self.lang
-            search.append(video_file_details)
-
-        results = self._query_opensubs(search)
-
-        subtitles = self.clean_results(results)
-        print(subtitles, "clean_results")
-        for hash, found_matches in subtitles.items():
-            for dicts in found_matches:
-                cnt[dicts[4]] += 1
-
-            most_common = cnt.most_common(1)[0][0]
-            expectedResult = [d for d in found_matches if d[4] == most_common]
-            print(expectedResult, "expected")
-            subtitles[hash] = sorted(found_matches, key=lambda x: (x[3], -x[2], -x[1]))[0]
-
-        for (hash, filedetails) in self.moviefiles.items():
-            if not self.stopping:
-                if subtitles.get(hash):
-                    print('Saving subtitles for %s' % filedetails['file_name'], 'success')
-                    subtitle = self.download_subtitles([subtitles[hash][0]])
-                    save_subs(subtitle, filedetails['save_subs_to'], subtitles[hash])
+                        b['best_match'] = 0
                 else:
-                    print("no sub found")
-            else:
-                return
+                    b['completed'] = 0
 
-    def download_subtitles(self, subparam):
-        resp = self.server.DownloadSubtitles(self.login_token, subparam)
-        self.check_status(resp)
-        decoded = base64.standard_b64decode(resp['data'][0]['data'].encode('ascii'))
-        decompressed = zlib.decompress(decoded, 15 + 32)
-        return decompressed
+                if subteams.issubset(teams):
+                    b['overlap'] = len(set.intersection(teams, subteams))
+                else:
+                    b['overlap'] = 0
+                result.append(b)
+        except:
+            print('Following unknown exception occured:\n%s' % traceback.format_exc(), 'error')
+
+        else:
+            if result:
+                # Sort the results found by completed, overlapping, best_match
+
+                best_match = sorted(result, key=itemgetter('completed', 'overlap', 'best_match'), reverse=True)[0]
+
+        return (searchurl, best_match.get('link') if best_match else None)
+
+    def download_subtitles(self, searchurl, link, filename):
+        referer = searchurl.replace('_', ' ')
+        print('Saving subtitles for %s...' % filename, 'success')
+        return download_url_content(link, referer)
 
 
 def process_queue(queue):
@@ -317,16 +337,17 @@ def process_queue(queue):
         if video['type'] == 'Addic7ed':
             addic7ed_list.append(video)
 
-    # if addic7ed_list:
-    #     catch_all('Addic7ed', addic7ed_list)
+    if addic7ed_list:
+        ad = Addic7ed()
+        ad.process(addic7ed_list)
 
     # time.sleep(0.01)  # To prevent race condition
 
-    for video in queue:
-        if video['type'] == 'OpenSubtitles':
-            opensubs_dict[video['moviehash']] = video
+    # for video in queue:
+    #     if video['type'] == 'OpenSubtitles':
+    #         opensubs_dict[video['moviehash']] = video
 
-    print(opensubs_dict)
+    # print(opensubs_dict)
     # if opensubs_dict:
 
     #     sub = OpenSubtitles()
@@ -373,7 +394,7 @@ def file_paths(input_path):          # mine
     return file_list
 
 
-input_path = "E:\\To Catch A Thief (1955) [BluRay] [720p] [YTS.AM]"
+input_path = "E:\\Citizen.Khan.S04E01.HDTV.x264-TLA[ettv]"
 
 check_and_add(file_paths(input_path))
 
